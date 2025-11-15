417dc7dad27dd5a1a85a786b871d411c227ba1df
9527 <1146699396@qq.com>
2025-11-15 17:42:05 +0800

    [feat]:获取本机IP

--- server/src/test/kotlin/org/example/project/ApplicationTest.kt
+++ server/src/test/kotlin/org/example/project/ApplicationTest.kt
@@ -5,6 +5,38 @@ import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
 import kotlin.test.*
+import java.net.Inet4Address
+import java.net.InetAddress
+import java.net.NetworkInterface
+import java.net.SocketException
+import java.util.Enumeration
+
+fun getLocalIpAddress(): String? {
+    try {
+        // 遍历所有网络接口
+        val interfaces: Enumeration<NetworkInterface> = NetworkInterface.getNetworkInterfaces()
+        while (interfaces.hasMoreElements()) {
+            val networkInterface: NetworkInterface = interfaces.nextElement()
+
+            // 过滤掉未启用的接口
+            if (!networkInterface.isUp) continue
+
+            // 遍历接口的所有IP地址
+            val addresses: Enumeration<InetAddress> = networkInterface.inetAddresses
+            while (addresses.hasMoreElements()) {
+                val inetAddress: InetAddress = addresses.nextElement()
+
+                // 筛选：IPv4 且 非回环地址
+                if (!inetAddress.isLoopbackAddress && inetAddress is Inet4Address) {
+                    return inetAddress.hostAddress
+                }
+            }
+        }
+    } catch (e: SocketException) {
+        e.printStackTrace()
+    }
+    return null // 未找到有效IP
+}
 
 class ApplicationTest {
 
@@ -17,4 +49,9 @@ class ApplicationTest {
         assertEquals(HttpStatusCode.OK, response.status)
         assertEquals("Ktor: ${Greeting().greet()}", response.bodyAsText())
     }
+}
+
+fun main() {
+    val address = getLocalIpAddress()
+    print(address)
 }
